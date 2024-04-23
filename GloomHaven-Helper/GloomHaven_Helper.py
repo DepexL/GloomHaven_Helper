27736ce3a2c0cd98293e8db834bb97dabf1d4c08
DepexL <liutauraskuzma@gmail.com>
2024-04-23 17:13:24 +0300

    The start of the code
    The start

---
+++ GloomHaven-Helper/GloomHaven_Helper.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+# why are u not working
+
+class App(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.title_label = tk.Label(self, text="Welcome to Gloomhaven", font=("Helvetica", 24))
+        self.title_label.pack(pady=20) 
+
+        self.exit_button = tk.Button(self, text="Exit", command=self.master.destroy)
+        self.exit_button.pack()
+
+myapp = App()
+
+myapp.master.title("My Do-Nothing Application")
+myapp.master.maxsize(1000, 400)
+
+myapp.mainloop()
