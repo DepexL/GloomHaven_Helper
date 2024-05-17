3ab94c9b447eea2d148ab1e94ec66c8ebac173e3
DepexL <liutauraskuzma@gmail.com>
2024-05-17 09:47:35 +0300

    Char classes and 3 page upgrade
    Pridjau visas zaidimo klases bei patobulinau 3 puslapi

--- GloomHaven-Helper/GloomHaven_Helper.py
+++ GloomHaven-Helper/GloomHaven_Helper.py
@@ -48,12 +48,79 @@ class Character_Creator:
         self.Char_name = Char_name
         self.Specialty = Specialty
         self.Level = Level
-        self.Health = Health
+        self.Health = 8
         self.Skills = []
         self.Items = []
         
     def add_skill(self, skill_name):
-        self.Skills.append(skill_name)
+        if skill_name not in self.Skills: 
+            self.Skills.append(skill_name)
+        
+class Voidwarden_class(Character_Creator):
+    def __init__(self, Char_name, Specialty, Health, Level, Mana):
+        super().__init__(Char_name, Specialty, Health, Level)
+        self.Mana = Mana  # New attribute specific to Character_Class
+
+
+    def add_skill(self, skill_name):
+        if skill_name not in self.Skills: 
+            self.Skills.append(skill_name)
+        else:
+            print(f"{skill_name} already exists in the skills list.")
+
+
+    def display_mana(self):
+        return f"Mana: {self.Mana}"
+    
+class Hatchet_class(Character_Creator):
+    def __init__(self, Char_name, Specialty, Health, Level, Stamina):
+        super().__init__(Char_name, Specialty, Health, Level)
+        self.Stamina = Stamina 
+
+
+    def add_skill(self, skill_name):
+        if skill_name not in self.Skills: 
+            self.Skills.append(skill_name)
+        else:
+            print(f"{skill_name} already exists in the skills list.")
+
+
+    def display_mana(self):
+        return f"Mana: {self.Mana}"
+    
+class RedGuard_class(Character_Creator):
+    def __init__(self, Char_name, Specialty, Health, Level, Rage):
+        super().__init__(Char_name, Specialty, Health, Level)
+        self.Rage = Rage 
+
+
+    def add_skill(self, skill_name):
+        if skill_name not in self.Skills: 
+            self.Skills.append(skill_name)
+        else:
+            print(f"{skill_name} already exists in the skills list.")
+
+
+    def display_mana(self):
+        return f"Mana: {self.Mana}"
+    
+
+class Demolitionist_class(Character_Creator):
+    def __init__(self, Char_name, Specialty, Health, Level, Mana):
+        super().__init__(Char_name, Specialty, Health, Level)
+        self.GunPowder = GunPowder  
+
+
+    def add_skill(self, skill_name):
+        if skill_name not in self.Skills: 
+            self.Skills.append(skill_name)
+        else:
+            print(f"{skill_name} already exists in the skills list.")
+
+
+    def display_mana(self):
+        return f"Mana: {self.Mana}"
+    
 
     
 # Klasiu kurimas kuriuos zaidejas gales pasirinkti
@@ -92,6 +159,8 @@ print("Player1 characters:", player1_characters)
 class App(customtkinter.CTk):
     def __init__(self):
         
+        self.player_tracker = PlayerTracker()
+        
         # Programos Atrodymas
         super().__init__()
         self.geometry("1200x800")
@@ -105,7 +174,6 @@ class App(customtkinter.CTk):
     def create_page1(self):
         
         project_logo = CTkImage(dark_image=Image.open("gloomhaven-logo.png"), size=(1000, 350))        
-
 
         self.logo_label = CTkLabel(self, image=project_logo, text="")
         self.logo_label.place(relx=0.5, rely=0.25, anchor="center")
@@ -190,32 +258,58 @@ class App(customtkinter.CTk):
 # Trecias puslapis - Zaidejas ir Zmogeliukai
     def create_page3(self):
         self.frame_1 = CTkFrame(master=self, fg_color="#e5d8ad", height=1200, width=750, border_color="#e5aaa7", border_width=2)
-        self.frame_1.pack(expand=True, fill="both", pady=(50, 0), padx=30, anchor="n")  # Adjusted pady and anchor
+        self.frame_1.pack(expand=True, fill="both", pady=(50, 20), padx=30, anchor="n")  # Adjusted pady and anchor
     
         self.frame_2 = CTkFrame(master=self.frame_1, fg_color="#e5d8ad", height=1200, width=750, border_color="#e5aaa7", border_width=0)
         self.frame_2.pack(expand=True, fill="x", pady=0, padx=30)
         
-        self.frame_3 = CTkFrame(master=self.frame_1, fg_color="black")
+        self.frame_3 = CTkFrame(master=self.frame_1, fg_color="#ECCE5C", border_color="black", border_width=2)
         self.frame_3.place(relx=0.5, rely=0.30, anchor="n")  # Center vertically within frame_1
+        
+        self.frame_4 = CTkFrame(master=self.frame_1, fg_color="#e5d8ad")
+        self.frame_4.place(relx=0.5, rely=0.40, anchor="n")  # Center vertically within frame_1
     
         self.desc_label = CTkLabel(master=self.frame_2, font=("Baskerville Old Face", 32, "bold"), text="Now Lets Create Players And Characters", text_color="black")
         self.desc_label.pack(anchor="n", expand=True, pady=(10, 400), padx= 10)    
 
-        self.desc_label = CTkLabel(master=self.frame_3, font=("Baskerville Old Face", 22, "bold"), text="Player Name:", text_color="red")
+# Solo Game selection stage 1.
+        self.desc_label = CTkLabel(master=self.frame_3, font=("Baskerville Old Face", 22, "bold"), text="Player Name:", text_color="black")
         self.desc_label.pack(side=LEFT, anchor="n", pady=20, padx=30)    
-        self.entry_name = CTkEntry(master=self.frame_3, placeholder_text="Start typing...")
-        self.btn_submit = CTkButton(master=self.frame_3, text="Submit", command=self.click_handler1)
+        
+        self.entry_name = CTkEntry(master=self.frame_3, placeholder_text="Start typing...", fg_color="#e5d8ad",font=("Baskerville Old Face", 22, "bold"), text_color="black" , width=200 )
         self.entry_name.pack(side=LEFT, anchor="n", pady=20, padx=30)
+        
+        self.desc_label = CTkLabel(master=self.frame_3, font=("Baskerville Old Face", 22, "bold"), text="How many characters: ", text_color="black")
+        self.desc_label.pack(side=LEFT, anchor="n", pady=20, padx=30)    
+        
+        self.combobox_players = customtkinter.CTkComboBox(master=self.frame_3, values=["1", "2", "3",
+                                                                                       "4"], font=("Baskerville Old Face", 22, "bold")
+                                                          , command=self.combobox_callback_charNumb, width=1, fg_color="#e5d8ad", text_color="black",
+                                                          dropdown_font=("Baskerville Old Face", 22, "bold"), hover=True)
+        self.combobox_players.set("1")
+        self.combobox_players.pack(expand=True, pady=10, padx= 10)      
+
+        
+        self.btn_submit = CTkButton(master=self.frame_4, font=("Baskerville Old Face", 18, "bold"), text="Submit", command=self.click_handler1, height=50, fg_color="#9c4541", hover_color="#79312d")
         self.btn_submit.pack(side=LEFT, anchor="n", pady=20, padx=30)
         
-        self.entry = CTkEntry(master=self.frame_3, placeholder_text="Start typing...")
-        self.btn5 = CTkButton(master=self.frame_3, text="Submit", command=self.click_handler1)
-        self.entry.pack(side=LEFT, anchor="n", pady=20, padx=30)
-        self.btn5.pack(side=LEFT, anchor="n", pady=20, padx=30)
 
+        self.charNumb = int(self.combobox_players.get())
 
+    def combobox_callback_charNumb(self, choice):
+        self.charNumb = int(choice)
+    
     def click_handler1(self):
-        print(f"Entered Value: {self.entry.get()}")
+        name = self.entry_name.get()
+        self.player_tracker.add_player(name)
+        for i in range(self.charNumb):
+            # add characters Names and skills and level
+            character_name = f"Character{i+1}"
+            self.player_tracker.add_character(name, character_name)
+        characters = self.player_tracker.get_characters(name)
+        print(f"Name: {name}, Characters: {characters}")
+        
+# Solo Game selection stage 2.
     
 
 # x puslapis - Load Game
